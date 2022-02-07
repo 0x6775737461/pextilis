908e185b3a6a2cb0373088a3f2ab1870ed001a11
0x6775737461 <fabiogustavo@tuta.io>
2022-02-07 18:26:14 -0400

    implementing auth_data() to setup all credentials

--- src/main.py
+++ src/main.py
@@ -2,14 +2,27 @@ from telethon import TelegramClient, events, sync
 from dotenv import load_dotenv
 import os
 
-# api credentials
-load_dotenv('.env')
+def auth_data(file_name: str) -> dict:
+    '''
+    take a file and grab all needed
+    credentials to use telethon
+    '''
 
-api_id = os.getenv("API_ID")
-api_hash = os.getenv("API_HASH")
-username = os.getenv("USERNAME")
+    # api credentials
+    load_dotenv(file_name)
 
-client = TelegramClient('pextilis', api_id, api_hash)
+    auth_keys = {
+        'api_id': os.getenv("API_ID"),
+        'api_hash': os.getenv("API_HASH"),
+        'username': os.getenv("USERNAME")
+    }
+
+    return auth_keys
+
+auth_keys = auth_data('.env')
+
+client = TelegramClient('pextilis', auth_keys['api_id'], auth_keys['api_hash'])
 client.start()
 
-client.send_message(username, 'Hello! Talking to you from Telethon')
+client.send_message(auth_keys['username'], 
+    'Hello! Talking to you from Telethon')
